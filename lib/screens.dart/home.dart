70faf1ceca8372198fe4c9b8e4269e05689df1fe
tatarmagda <tatar.magdalena@wp.pl>
2022-01-23 13:09:57 +0100

    App Bar is finished

--- lib/screens.dart/home.dart
+++ lib/screens.dart/home.dart
@@ -7,10 +7,25 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            centerTitle: true,
-            title: MyTextWidget(
-              text: "todoapp",
-            )));
+      appBar: AppBar(
+        centerTitle: true,
+        title: MyTextWidget(
+          text: "todoapp",
+          color: Colors.white54,
+        ),
+        //actions - dodaje więcej (tak jak plusik) wszytsko po prawej stronie w appbarze
+        actions: [
+          IconButton(
+              onPressed: () {
+                print("nie działa");
+              },
+              icon: Icon(
+                Icons.add,
+              ))
+        ],
+        // (){} - pusta funkcja
+      ),
+      body: ListView(),
+    );
   }
 }
